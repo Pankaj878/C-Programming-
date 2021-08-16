6aa75cf6edfe2c8bf88cab9426162541f6100d81
Pankaj Kaurav <84280158+Pankaj878@users.noreply.github.com>
2021-08-16 21:54:56 +0530

    Add files via upload

---
+++ basicprograme.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+    printf("Result=%d\n",a+b);
+    c=a+b;
+    printf("average=%d",c/2);
+}
