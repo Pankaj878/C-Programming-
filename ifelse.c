dd8331439d2dd6a5ff192cdbbefc5394c7177ca0
Pankaj Kaurav <84280158+Pankaj878@users.noreply.github.com>
2021-08-21 21:56:06 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int a;
+  printf("Enter your choice\n1=science and math\n2=science\n3=math\nYour choice:");
+  scanf("%d",&a);
+  if (a==1)
+  {
+  printf("45");}
+  else if (a==2)
+  printf("15");
+  else if (a==3)
+  printf("15");
+  else
+  printf("bhag");
+}
